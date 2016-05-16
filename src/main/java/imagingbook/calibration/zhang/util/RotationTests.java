de92fb905971e48851c9286b158df4090c24475d
imagingbook <wilhelm.burger@fh-hagenberg.at>
2016-05-16 16:41:28 +0200

    finished RotationTests.java

--- src/main/java/imagingbook/calibration/zhang/util/RotationTests.java
+++ src/main/java/imagingbook/calibration/zhang/util/RotationTests.java
@@ -4,7 +4,15 @@ import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
 import org.apache.commons.math3.geometry.euclidean.threed.RotationConvention;
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 
-import imagingbook.lib.math.Arithmetic;
+import static imagingbook.lib.math.Arithmetic.EPSILON_DOUBLE;
+import static imagingbook.lib.math.Arithmetic.isZero;
+
+import static imagingbook.lib.math.Matrix.normL2;
+import static imagingbook.lib.math.Matrix.zeroVector;
+import static imagingbook.lib.math.Matrix.idMatrix;
+import static imagingbook.lib.math.Matrix.add;
+import static imagingbook.lib.math.Matrix.multiply;
+
 import imagingbook.lib.math.Matrix;
 import imagingbook.lib.settings.PrintPrecision;
 
@@ -13,8 +21,10 @@ public class RotationTests {
 	public static void main(String[] args) {
 		PrintPrecision.set(6);
 
-		double[] axis = {1.5, 0.25, -0.5}; //{1,1,1};
-		double theta = Matrix.normL2(axis); //0.2;
+		double[] axis = {0, -5, 0}; //{1,1,1};
+		double theta = normL2(axis); //0.2;
+		
+		System.out.println("*** Rodrigues vector -> Rotation matrix:\n");
 		
 		double[] rv = makeRodriguesVector(axis, theta);
 		System.out.println("original rv = " + Matrix.toString(rv));
@@ -29,22 +39,22 @@ public class RotationTests {
 		System.out.println();
 		
 		//-- going back:
-		System.out.println();
+		System.out.println("*** Rotation matrix -> Rodrigues vector:\n");
 		
 		double[] rv1 = toRodriguesVector1(R1);
 		System.out.println("rv1 = " + Matrix.toString(rv1));
-		System.out.println("theta1 = " + Matrix.normL2(rv1));
+		System.out.println("theta1 = " + normL2(rv1));
 		System.out.println();
 		
 		double[] rv2 = toRodriguesVector2(R1);
 		System.out.println("rv2 = " + Matrix.toString(rv2));
-		System.out.println("theta2 = " + Matrix.normL2(rv2));
+		System.out.println("theta2 = " + normL2(rv2));
 		System.out.println();
 	}
 	
 	static double[] makeRodriguesVector(double[] axis, double theta) {
-		double s = Matrix.normL2(axis);
-		return Matrix.multiply(theta / s, axis);
+		double s = normL2(axis);
+		return multiply(theta / s, axis);
 	}
 	
 	// ++++++++++++++   Rodrigues vector --> Rotation matrix  +++++++++++++++++++
@@ -55,7 +65,7 @@ public class RotationTests {
 	 * @return
 	 */
 	static double[][] toRotationMatrix1(double[] rv) {
-		double theta = Matrix.normL2(rv);
+		double theta = normL2(rv);
 		double rx = rv[0] / theta;
 		double ry = rv[1] / theta;
 		double rz = rv[2] / theta;
@@ -68,11 +78,10 @@ public class RotationTests {
 				{rz, 0, -rx},
 				{-ry, rx, 0}};
 		//System.out.println("W = \n" + Matrix.toString(W));
-		double[][] I = Matrix.createIdentityMatrix(3);
-		double[][] R1 = Matrix.add(I, Matrix.multiply(Math.sin(theta), W));
-		//double[][] R2 = Matrix.multiply(1 - Math.cos(theta),Matrix.multiply(Matrix.transpose(W),W));
-		double[][] R2 = Matrix.multiply(1 - Math.cos(theta),Matrix.multiply(W,W));
-		double[][] R = Matrix.add(R1, R2);
+		double[][] I = idMatrix(3);
+		double[][] R1 = add(I, multiply(Math.sin(theta), W));
+		double[][] R2 = multiply(1 - Math.cos(theta), multiply(W,W));
+		double[][] R = add(R1, R2);
 		
 		return R;
 	}
@@ -83,11 +92,11 @@ public class RotationTests {
 	 * @return
 	 */
 	static double[][] toRotationMatrix2(double[] rv) {
-		double angle = Matrix.normL2(rv);
+		double angle = normL2(rv);
 		Vector3D axis = new Vector3D(rv);
 		Rotation rotation = new Rotation(axis, angle, RotationConvention.VECTOR_OPERATOR);
-		System.out.println("rotation angle3 = " + rotation.getAngle());
-		System.out.println("rotation axis3 = " + rotation.getAxis(RotationConvention.VECTOR_OPERATOR));
+		System.out.println("rotation angle2 = " + rotation.getAngle());
+		System.out.println("rotation axis2 = " + rotation.getAxis(RotationConvention.VECTOR_OPERATOR));
 		return rotation.getMatrix();
 	}
 	
@@ -101,69 +110,62 @@ public class RotationTests {
 	 * @return
 	 */
 	static double[] toRodriguesVector1(double[][] R) {
-		final double eps = Arithmetic.EPSILON_DOUBLE;
-		final double small = Math.sqrt(eps);
-		final double tiny = eps * 100;
+		final double eps = EPSILON_DOUBLE;
 		
-		double[] rho = {
+		double[] p = {
 				0.5 * (R[2][1] - R[1][2]), 
 				0.5 * (R[0][2] - R[2][0]), 
 				0.5 * (R[1][0] - R[0][1])};
-		double sn = Matrix.normL2(rho);
-		double cs = (Matrix.trace(R) - 1) / 2;
+		double s = normL2(p);
+		double c = 0.5 * (Matrix.trace(R) - 1);
 		
-		if (sn < eps) {                      // Rotation angle is either 0 or pi
-			if (Math.abs(cs - 1) < tiny) {       // c = 1, Rotation angle is 0
-				return new double[] {0, 0, 0};
+		if (isZero(s)) {                      // Rotation angle is either 0 or pi
+			if (isZero(c - 1)) {       // Case 1: c = 1, Rotation angle is 0
+				return zeroVector(3);
 			}
-			else if (Math.abs(cs + 1) < tiny) {    // c = -1, Rotation angle is pi
-				// Find the column of R + I with greatest norm (for better numerical results)
-				//		            Rp = R + eye(3);
-				double[][] Rp = Matrix.add(R, Matrix.createIdentityMatrix(3));
-				//		            colNorm2 = diag(Rp'*Rp);
-				//		            [val, col] = max(colNorm2);
+			else if (isZero(c + 1)) {    // Case 2: c = -1, Rotation angle is pi
+				// find the column of R + I with greatest norm (for better numerical results)
+				double[][] Rp = add(R, idMatrix(3));
 				double[] v = getMaxColumnVector(Rp);
-				double val = Matrix.normL2(v);
-				if (val < small) {         // Shouldn't really happen: R == -eye(3)
+				double vn = normL2(v);
+				if (isZero(vn)) {   // this shouldn't really happen
 					throw new RuntimeException("R is an inversion, not a rotation");
 				}
-				//		            v = Rp(:, col);
-				//		            u = v / norm(v);
-				//		            rOut = Math.PI * hemisphere(u);
-				double[] u = Matrix.multiply(1 / val, v);
-				return Matrix.multiply(Math.PI, S(u));
+				double[] u = multiply(1 / vn, v);	// unit vector
+				return multiply(Math.PI, S(u));
 			}
-			else {                        // How can this be?
+			else {                  // how can this be?
 				throw new RuntimeException("sin(theta) is zero, bus cos(theta) is neither 1 nor -1!");
 			}
 		}
-		else  {                          // Rotation strictly between 0 and pi
-			double[] u = Matrix.multiply(1 / sn, rho);
-			double theta = Math.atan2(sn, cs);
-			return Matrix.multiply(theta, u);
+		else  {   // (s != 0) : rotation strictly between 0 and pi
+			double[] u = multiply(1 / s, p);	// unit vector
+			double theta = Math.atan2(s, c);
+			return multiply(theta, u);
 		}
 	}
 	
 	/**
 	 * Changes the sign of a unit vector u so that it is on the proper half of
 	 * the unit sphere.
-	 * @param u unit vector
+	 * @param x unit vector
 	 * @return same or inverted unit vector
 	 */
-	private static double[] S(double[] u) {
+	private static double[] S(double[] x) {
 //		if ((u[0] == 0 && u[1] == 0 && u[2] < 0) || (u[0] == 0 && u[1] < 0) || u[0] < 0) 
-		if ((isZero(u[0]) && isZero(u[1]) && u[2] < 0) || (isZero(u[0]) && u[1] < 0) || u[0] < 0)
-		{
-			return Matrix.multiply(-1, u);
+		if ((x[0] < 0) ||
+			(isZero(x[0]) && x[1] < 0) ||
+			(isZero(x[0]) && isZero(x[1]) && x[2] < 0))	{
+			return multiply(-1, x);
 		}
 		else {
-			return u;
+			return x;
 		}
 	}
 	
-	private static boolean isZero(double x) {
-		return Math.abs(x) < Arithmetic.EPSILON_DOUBLE;
-	}
+//	private static boolean isZero(double x) {
+//		return Math.abs(x) < EPSILON_DOUBLE;
+//	}
 	
 	private static double[] getMaxColumnVector(double[][] A) {
 		final int rows = A.length;
