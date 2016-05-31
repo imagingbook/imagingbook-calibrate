f10e7334385e08b86dddfa72bf0070d8df6bff7f
Wilhelm Burger <wilbur@ieee.org>
2016-05-31 18:14:16 +0200

    a

--- src/main/java/Calibration_Plugins/Open_Test_Images.java
+++ src/main/java/Calibration_Plugins/Open_Test_Images.java
@@ -1,10 +1,18 @@
 package Calibration_Plugins;
 
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Path;
+
+import ij.IJ;
 import ij.ImagePlus;
 import ij.io.Opener;
 import ij.plugin.PlugIn;
 import imagingbook.calibration.zhang.testdata.ZhangData;
 import imagingbook.lib.ij.IjLogStream;
+import imagingbook.lib.util.FileUtils;
 
 /**
  * Opens Zhang's standard calibration images as a stack of RGB images. 
@@ -26,6 +34,32 @@ public class Open_Test_Images implements PlugIn {
 
 	public void run(String arg0) {
 		String path = ZhangData.getResourcePath(resourceName);
+		IJ.log("resource path = " + path);
+		try {
+			IJ.log("resource URL = " + new URL("file:" + path));
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// ------------------------------------------
+		
+		Path[] paths = FileUtils.listResources(ZhangData.class, "resources");
+		for (Path p : paths) {
+			IJ.log("path = " + p.toString());
+			
+		}
+		
+		InputStream strm = FileUtils.getResourceStream(ZhangData.class, "resources/" + resourceName);
+		IJ.log("stream = " + strm);
+		
+		Path p2 = FileUtils.getResourcePath2(ZhangData.class, "resources/" + resourceName);
+		IJ.log("path 2 = " + p2);
+	
+
+		
+		// ------------------------------------------
+		
 		if (path == null) {
 			System.out.println("Image not found: " + resourceName);
 			return;
