b52793f9f1046952389178c5eddbce906b0abe2c
imagingbook <wilbur@ieee.org>
2022-04-23 11:16:39 +0200

    restructured project (merged data into lib), new POM setup, stepped to 3.2.0-SNAPSHOT

---
+++ imagingbook-calibrate-lib/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"imagingbook.calibration.data.zhang"},{"l":"imagingbook.calibration.zhang"},{"l":"imagingbook.calibration.zhang.util"}]
---
+++ imagingbook-calibrate-plugins/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"Calibration_Demos"},{"l":"Calibration_Demos_More"}]
