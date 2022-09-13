90ab16945996d588257575ac70e5ba2fe327b11f
imagingbook <mail.wilhelm.burger@gmail.com>
2022-09-13 21:16:38 +0200

    ParameterBundle/DialogParameterBundle: revised

--- imagingbook-calibrate-lib/src/main/java/imagingbook/calibration/zhang/Calibrator.java
+++ imagingbook-calibrate-lib/src/main/java/imagingbook/calibration/zhang/Calibrator.java
@@ -8,7 +8,7 @@ import org.apache.commons.math3.linear.RealMatrix;
 
 import imagingbook.calibration.zhang.util.MathUtil;
 import imagingbook.common.math.Matrix;
-import imagingbook.common.util.parameters.DialogParameters;
+import imagingbook.common.util.parameters.DialogParameterBundle;
 
 
 /**
@@ -34,7 +34,7 @@ public class Calibrator {
 	 * new objects of type of {@link Calibrator}. 
 	 * Parameters can be specified by setting the associated public fields.
 	 */
-	public static class Parameters implements DialogParameters {
+	public static class Parameters implements DialogParameterBundle {
 		/** Normalize point coordinates for numerical stability in {@link HomographyEstimator}. */
 		public boolean normalizePointCoordinates = true;
 		/** Assume that the camera has no skew (currently not used). */
