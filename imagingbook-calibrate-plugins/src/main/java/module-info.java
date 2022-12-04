22d915ac6b8b9982edadda2ec08f75494133f4c2
imagingbook <wilbur@ieee.org>
2022-12-04 09:51:40 +0100

    removed local 'javadoc'

--- imagingbook-calibrate-plugins/src/main/java/module-info.java
+++ imagingbook-calibrate-plugins/src/main/java/module-info.java
@@ -1,4 +1,7 @@
 module imagingbook.calibrate.plugins {
+    exports Calibration_Demos;
+    exports Calibration_Demos_More;
+
     requires java.desktop;
     requires imagingbook.calibrate.lib;
     requires imagingbook.common;
