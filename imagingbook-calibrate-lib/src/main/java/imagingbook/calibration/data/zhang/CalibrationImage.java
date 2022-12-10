e337b8c5667c31ee714b024e749a96d215483c9a
imagingbook <wilbur@ieee.org>
2022-12-10 10:56:02 +0100

    Sample images renamed, using auto-naming for PNGs

--- imagingbook-calibrate-lib/src/main/java/imagingbook/calibration/data/zhang/CalibrationImage.java
+++ imagingbook-calibrate-lib/src/main/java/imagingbook/calibration/data/zhang/CalibrationImage.java
@@ -3,40 +3,37 @@ package imagingbook.calibration.data.zhang;
 import imagingbook.core.resource.ImageResource;
 
 public enum CalibrationImage implements ImageResource {
-		CalibIm1_png("CalibIm1.png"),
-		CalibIm2_png("CalibIm2.png"),
-		CalibIm3_png("CalibIm3.png"),
-		CalibIm4_png("CalibIm4.png"),
-		CalibIm5_png("CalibIm5.png"),
-		marked1_png("marked1.png"),
-		marked2_png("marked2.png"),
-		marked3_png("marked3.png"),
-		marked4_png("marked4.png"),
-		marked5_png("marked5.png"),
-		CalibImageStack_tif("CalibImageStack.tif");
+	CalibImageStack("CalibImageStack.tif"),
+	CalibIm1,
+	CalibIm2,
+	CalibIm3,
+	CalibIm4,
+	CalibIm5,
+	marked1,
+	marked2,
+	marked3,
+	marked4,
+	marked5;
 
 	private final static String BASEDIR = "DATA";
 	private final String filename;
-	
+
+	CalibrationImage() {
+		this(null);
+	}
+
 	CalibrationImage(String filename) {
 		this.filename = filename;
 	}
-	
+
 	@Override
 	public String getFileName() {
-		return filename;
+		return (this.filename != null) ? this.filename : this.autoName();
 	}
 	
 	@Override
 	public String getRelativeDirectory() {
 		return BASEDIR;
 	}
-	
-	
-//	public static void main(String[] args) {
-//		for (ImageResource ir : CalibrationImage.values()) {
-//			System.out.println(ir.getImage().toString());
-//		}
-//	}
 
 }
