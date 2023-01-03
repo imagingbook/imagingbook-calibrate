2bdfbc88a3eb5880dcf8f2ccdc636ce45e77eb58
imagingbook <wilbur@ieee.org>
2023-01-03 14:54:23 +0100

    copyright update

--- imagingbook-calibrate-plugins/src/main/java/Calibration_Plugins_2/Draw_3D_Axes_Demo.java
+++ imagingbook-calibrate-plugins/src/main/java/Calibration_Plugins_2/Draw_3D_Axes_Demo.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Permission to use and distribute this software is granted under the BSD 2-Clause
  * "Simplified" License (see http://opensource.org/licenses/BSD-2-Clause).
- * Copyright (c) 2016-2022 Wilhelm Burger. All rights reserved.
+ * Copyright (c) 2016-2023 Wilhelm Burger. All rights reserved.
  * Visit https://imagingbook.com for additional details.
  ******************************************************************************/
 package Calibration_Plugins_2;
