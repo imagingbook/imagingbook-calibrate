25cb5088fec2cca1e71678573b2494f0b1c913e9
imagingbook <wilbur@ieee.org>
2025-10-25 08:42:07 +0200

    Rebuild to 7.2.0

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-mvn clean 
-mvn install -Dmaven.test.skip=true
-
-rm -rf javadoc/*
-mvn javadoc:aggregate -Dimagingbook.skipjavadoc=false
+mvn clean install -Dmaven.test.skip=true
 
 echo ""
 read -rsp $'Done. Press any key to quit...\n' -n1 key
