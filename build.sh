590257231481dbd50c57fa7868245d0fc1d8a352
imagingbook <wilbur@ieee.org>
2025-10-29 15:12:12 +0100

    Minor edit

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 mvn clean install -Dmaven.test.skip=true
+# mvn clean install -Dimagingbook.skipjavadoc=true
+# mvn clean install -Dmaven.test.skip=true -Dimagingbook.skipjavadoc=true
 
 echo ""
 read -rsp $'Done. Press any key to quit...\n' -n1 key
