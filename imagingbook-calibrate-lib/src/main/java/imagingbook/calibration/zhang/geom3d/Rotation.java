92f6f6edc6ab842e23eb69c66ad9cbb4f2c783fb
imagingbook <wilbur@ieee.org>
2025-10-30 11:50:20 +0100

    Made org.apache.commons.numbers.arrays.LinearCombination a local class, being abandoned after version 1.0-beta1

--- imagingbook-calibrate-lib/src/main/java/imagingbook/calibration/zhang/geom3d/Rotation.java
+++ imagingbook-calibrate-lib/src/main/java/imagingbook/calibration/zhang/geom3d/Rotation.java
@@ -9,7 +9,7 @@ package imagingbook.calibration.zhang.geom3d;
 import imagingbook.calibration.zhang.util.MathUtil;
 import org.apache.commons.geometry.euclidean.threed.Vector3D;
 import org.apache.commons.math4.core.jdkmath.JdkMath;
-import org.apache.commons.numbers.arrays.LinearCombination;
+//import org.apache.commons.numbers.arrays.LinearCombination;
 
 import java.io.Serializable;
 import java.util.Arrays;
@@ -301,21 +301,21 @@ public class Rotation implements Serializable {
 
         // buid a matrix transforming the first base into the second one
         final double[][] m = new double[][] {
-                {
-                        LinearCombination.value(u1.getX(), v1.getX(), u2.getX(), v2.getX(), u3.getX(), v3.getX()),
-                        LinearCombination.value(u1.getY(), v1.getX(), u2.getY(), v2.getX(), u3.getY(), v3.getX()),
-                        LinearCombination.value(u1.getZ(), v1.getX(), u2.getZ(), v2.getX(), u3.getZ(), v3.getX())
-                },
-                {
-                        LinearCombination.value(u1.getX(), v1.getY(), u2.getX(), v2.getY(), u3.getX(), v3.getY()),
-                        LinearCombination.value(u1.getY(), v1.getY(), u2.getY(), v2.getY(), u3.getY(), v3.getY()),
-                        LinearCombination.value(u1.getZ(), v1.getY(), u2.getZ(), v2.getY(), u3.getZ(), v3.getY())
-                },
-                {
-                        LinearCombination.value(u1.getX(), v1.getZ(), u2.getX(), v2.getZ(), u3.getX(), v3.getZ()),
-                        LinearCombination.value(u1.getY(), v1.getZ(), u2.getY(), v2.getZ(), u3.getY(), v3.getZ()),
-                        LinearCombination.value(u1.getZ(), v1.getZ(), u2.getZ(), v2.getZ(), u3.getZ(), v3.getZ())
-                }
+            {
+                    LinearCombination.value(u1.getX(), v1.getX(), u2.getX(), v2.getX(), u3.getX(), v3.getX()),
+                    LinearCombination.value(u1.getY(), v1.getX(), u2.getY(), v2.getX(), u3.getY(), v3.getX()),
+                    LinearCombination.value(u1.getZ(), v1.getX(), u2.getZ(), v2.getX(), u3.getZ(), v3.getX())
+            },
+            {
+                    LinearCombination.value(u1.getX(), v1.getY(), u2.getX(), v2.getY(), u3.getX(), v3.getY()),
+                    LinearCombination.value(u1.getY(), v1.getY(), u2.getY(), v2.getY(), u3.getY(), v3.getY()),
+                    LinearCombination.value(u1.getZ(), v1.getY(), u2.getZ(), v2.getY(), u3.getZ(), v3.getY())
+            },
+            {
+                    LinearCombination.value(u1.getX(), v1.getZ(), u2.getX(), v2.getZ(), u3.getX(), v3.getZ()),
+                    LinearCombination.value(u1.getY(), v1.getZ(), u2.getY(), v2.getZ(), u3.getY(), v3.getZ()),
+                    LinearCombination.value(u1.getZ(), v1.getZ(), u2.getZ(), v2.getZ(), u3.getZ(), v3.getZ())
+            }
         };
 
         double[] quat = mat2quat(m);
