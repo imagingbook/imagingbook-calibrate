8f1095d22f51cbbe0a790feabdd1e101eea89f81
imagingbook <wilbur@ieee.org>
2025-11-01 17:54:48 +0100

    Added package-info

--- imagingbook-calibrate-lib/src/main/java/imagingbook/calibration/zhang/util/package-info.java
+++ imagingbook-calibrate-lib/src/main/java/imagingbook/calibration/zhang/util/package-info.java
@@ -7,7 +7,7 @@
  ******************************************************************************/
 
 /**
- * Various utility classes used by the calibration library.
+ * Utility classes used by the calibration library.
  * @author W. Burger
  */
 package imagingbook.calibration.zhang.util;
