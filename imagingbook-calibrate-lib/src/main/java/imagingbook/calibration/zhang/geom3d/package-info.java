05590553b16eb1a9fe8843c4c9bfff0dc73fa756
imagingbook <wilbur@ieee.org>
2025-11-01 17:56:08 +0100

    Added package-info

---
+++ imagingbook-calibrate-lib/src/main/java/imagingbook/calibration/zhang/geom3d/package-info.java
@@ -0,0 +1,11 @@
+/*******************************************************************************
+ * Permission to use and distribute this software is granted under the BSD 2-Clause
+ * "Simplified" License (see http://opensource.org/licenses/BSD-2-Clause).
+ * Copyright (c) 2016-2025 Wilhelm Burger. All rights reserved.
+ * Visit https://imagingbook.com for additional details.
+ ******************************************************************************/
+
+/**
+ * 3D rotation-related classes ported from {@code }org.apache.commons.math3.geometry.euclidean}
+ */
+package imagingbook.calibration.zhang.geom3d;
